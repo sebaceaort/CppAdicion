217e9321cd0187209a9fe84994b665af654c2f3d
sebaceaort <sceaort@gmail.com>
2017-04-06 14:53:09 -0300

    Add files via upload

---
+++ adicion.cpp
@@ -0,0 +1,17 @@
+//Nombre : adicion.cpp
+//Alumno : Sebastian Cea
+//Legajo : 1580048
+//Curso  : k1051
+//Fecha  : 20170406
+
+#include<iostream>
+
+int main(){
+int num1;
+int num2;
+        std::cout<<"ingrese el primer numero\n";
+        std::cin>>num1;
+        std::cout<<"ingrese el segundo numero\n";
+        std::cin>>num2;
+        std::cout<<"el resultado es  "<<num1+num2;
+}
